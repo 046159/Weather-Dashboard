32a3794c1c924a6e229a5221c86165d9832f5abe
046159 <43796736+046159@users.noreply.github.com>
2024-01-25 17:28:02 +0000

    Added code to get the city name entered and then call the Weather API to get the coordinates of the city

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/* ------------------------------- My API key ------------------------------- */
+var APIKey = "863baffad00b5904efb1c287625183c4";
+
+/* ---------------- Create a listener for the Search button ----------------- */
+document.getElementById("search-button").addEventListener("click", displayCity);
+function displayCity(event) {
+    event.preventDefault();
+    console.log(`The button with ID "${event.srcElement.id}" was pressed.`);
+    var city = document.getElementById("search-input").value;
+    console.log(`Input field has value "${city}".`)
+
+    /* ------------------- Get Latitude and Longitude for City ------------------ */
+    var queryURLCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
+    console.log(`Query URL for coordinates is: ${queryURLCoordinates}`);
+    fetch(queryURLCoordinates)
+        .then(function (response) {
+            // Calling .json() to access the json data stored inside the returned promise
+            return response.json();
+        })
+        // We store all of the retrieved data inside of an object called "data"
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            console.log(`Lat is ${lat}`);
+            console.log(`Lon is ${lon}`);
+        });
+
+    /* -------------------- Get the weather info for the city ------------------- */
+    var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
+    console.log(`Query URL for coordinates is: ${queryURLWeather}`);
+
+}
