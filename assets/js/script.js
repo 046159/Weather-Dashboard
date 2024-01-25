ea84a57ac41af0fa6ec35aa81fdee8bfcea0a314
046159 <43796736+046159@users.noreply.github.com>
2024-01-25 20:18:37 +0000

    Fixed problem where forecast for next 5 days was not being retrieved correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ function displayCity(event) {
 
     /* ------------------- Get Latitude and Longitude for City ------------------ */
     var queryURLCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
-    console.log(`Query URL for coordinates is: ${queryURLCoordinates}`);
+    // console.log(`Query URL for coordinates is: ${queryURLCoordinates}`);
     fetch(queryURLCoordinates)
         .then(function (response) {
             // Calling .json() to access the json data stored inside the returned promise
@@ -26,38 +26,38 @@ function displayCity(event) {
 
             /* -------------------- Get the weather info for the city ------------------- */
             var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
-            console.log(`Query URL for coordinates is: ${queryURLWeather}`);
+            // console.log(`Query URL for coordinates is: ${queryURLWeather}`);
 
 
             fetch(queryURLWeather)
-            .then(function (response) {
-                // Calling .json() to access the json data stored inside the returned promise
-                return response.json();
-            })
-            // We store all of the retrieved data inside of an object called "data"
-            .then(function (data) {
+                .then(function (response) {
+                    // Calling .json() to access the json data stored inside the returned promise
+                    return response.json();
+                })
+                // We store all of the retrieved data inside of an object called "data"
+                .then(function (data) {
+                    for (var i = 0; i < 40; i++) {
+                        // console.log(`${i} =============================================`);
+                        var unixDate = (data.list[i].dt) * 1000;
+                        var trueDate = new Date(unixDate);
 
-                for (var i = 0; i < 6; i++) {
-                    console.log("=============================================");
-                    console.log(`City name: ${data.city.name}`);
-                    console.log(`Date: ${data.list[i].dt}`);
-                    console.log(`Icon: ${data.list[i].weather[0].icon}`);
-                    console.log(`Temperature: ${data.list[i].main.temp}`);
-                    console.log(`Humidity: ${data.list[i].main.humidity}`);
-                    console.log(`Wind speed: ${data.list[i].wind.speed}`);
-                    
-                }
+                        var dateArray = trueDate.toString().split(" ");
+                        var dateDisplay = dateArray[0] + " " + dateArray[1] + " " + dateArray[2] + " " + dateArray[3];
+                        if ((dateArray[4] === "12:00:00") | (i === 0)) {
+                            console.log(`${i} =============================================`);
+                            console.log(`Date: ${dateDisplay}`);
+                            console.log(`City name: ${data.city.name}`);
+                            console.log(`Icon: ${data.list[i].weather[0].icon}`);
 
-
-
-        
-
-
-            });
-
-
+                            tempC = Math.round(data.list[i].main.temp - 273.15);
+                            console.log(`Temperature: ${tempC} Celcius`);
+                            console.log(`Humidity: ${data.list[i].main.humidity} %`);
+                            
+                            // Convert from metres per second to miles per hour
+                            mph = (data.list[i].wind.speed*2.23694);
+                            console.log(`Wind speed: ${data.list[i].wind.speed} MPH`);
+                        }
+                    }
+                });
         });
-
-
-
 }
